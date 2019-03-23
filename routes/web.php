327ecba18a9ad24550bf2ac74ad641628017260f
pankeel <pankeel@cognitivo.in>
2019-03-23 18:24:00 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::group(['middleware' => 'auth'], function () {
     
         Route::prefix('commercial')->group(function () {
             Route::prefix('reports')->group(function () {
+                 Route::get('PRY/hechauka/{start_date}/{end_date}', 'API\PRY\HechaukaController@generateFiles');
                 Route::get('purchases/{strDate}/{endDate}/{e?}', 'ReportController@purchases')->name('reports.purchases');
                 Route::get('purchases-byVAT/{strDate}/{endDate}/{e?}', 'ReportController@purchasesByVAT')->name('reports.purchaseByVAT');
                 Route::get('purchases-bySupplier/{strDate}/{endDate}/', 'ReportController@purchasesBySupplier')->name('reports.purchaseBySupplier');
