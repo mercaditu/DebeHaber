502ab6007e927b7e96f0597a31bf5a9071ecdf90
Abhishek Shah <abhi@cognitivo.in>
2019-04-08 20:24:16 -0400

    minor ui update

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -1,60 +1,64 @@
+<li class="nav-item dropdown">
+    <b-dropdown id="dropdown-1" :text="currentTeam.name" variant="outline-muted" size="sm">
+        <b-nav-item class="sub-menu" :to="{ name: 'taxPayer'}">
+            <i class="material-icons md-18 ml-10 mr-10">dashboard</i>
+            @{{ $t('general.dashBoard') }}
+        </b-nav-item>
+
+        <b-dropdown-divider></b-dropdown-divider>
+
+        <h3 class="nav-heading sub">
+            @{{ $t('general.configuration') }}
+        </h3>
+        <b-nav-item href="/settings/{{ Spark::teamsPrefix() }}/{{ \Auth::user()->currentTeam->id }}" class="sub-menu" v-b-tooltip.hover title="Team Settings">
+            <i class="material-icons md-18 ml-10 mr-10">settings</i>
+            @{{ $t('general.teamSettings') }}
+        </b-nav-item>
+    </b-dropdown>
+</li>
+
 @isset($taxPayerData)
     <!-- Left Side Of Navbar -->
-<li class="nav-item dropdown">
-    <a href="#" class="d-block d-md-flex text-center nav-link dropdown-toggle" id="dropdownTaxpayer" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <img :src="user.photo_url" class="dropdown-toggle-image" alt="Img"/>
-        <span> @{{ spark.taxPayerData.name }} </span>
-    </a>
-    <div class="dropdown-menu dropdown-menu-left" aria-labelledby="dropdownTaxpayer">
-        <b-nav-item class="sub-menu" :to="{ name: 'taxPayer'}">
+    <li class="nav-item">
+        <b-dropdown id="dropdown-1" :text="spark.taxPayerData.alias" variant="outline-success" size="sm">
+            <b-nav-item class="sub-menu" :to="{ name: 'taxPayer'}">
                 <i class="material-icons md-18 ml-10 mr-10">dashboard</i>
                 @{{ $t('general.dashBoard') }}
             </b-nav-item>
+
+            <b-dropdown-divider></b-dropdown-divider>
+
             <h3 class="nav-heading sub">
                 @{{ $t('general.configuration') }}
             </h3>
-            <b-nav vertical>
-                {{-- :href="/taxpayer/' + spark.taxPayerData.id" --}}
-                <b-nav-item class="sub-menu" href="/{{ $taxPayerData->id }}/{{ $currentCycle->id }}/taxpayer-integration/{{ $integrationType->id }}">
-                    <i class="material-icons md-18 ml-10 mr-10">settings</i>
-                    @{{ $t('general.taxPayer') }}
-                </b-nav-item>
-                <b-nav-item class="sub-menu" :to="{ name: 'documentList'}">
-                    <i class="material-icons md-18 ml-10 mr-10">file_copy</i>
-                    @{{ $t('commercial.documents') }}
-                </b-nav-item>
-                <b-nav-item class="sub-menu" :to="{ name: 'rateList'}">
-                    <i class="material-icons md-18 ml-10 mr-10">public</i>
-                    @{{ $t('commercial.exchangeRates') }}
-                </b-nav-item>
-            </b-nav>
-            <h3 class="nav-heading sub">
-                {{__('teams.teams')}}: @{{ currentTeam.name }}
-            </h3>
-            <b-nav vertical>
-                <b-nav-item href="/home" class="sub-menu" v-b-tooltip.hover title="Team Dashboard">
-                    <i class="material-icons md-18 ml-10 mr-10">dashboard</i>
-                    @{{ $t('general.teamDashBoard') }}
-                </b-nav-item>
-    
-                <b-nav-item href="/settings/{{ Spark::teamsPrefix() }}/{{ \Auth::user()->currentTeam->id }}" class="sub-menu" v-b-tooltip.hover title="Team Settings">
-                    <i class="material-icons md-18 ml-10 mr-10">settings</i>
-                    @{{ $t('general.teamSettings') }}
-                </b-nav-item>
-            </b-nav>
-            <hr>
-            <b-nav-item href="/home" class="sub-menu">
+            <b-nav-item class="sub-menu" href="/{{ $taxPayerData->id }}/{{ $currentCycle->id }}/taxpayer-integration/{{ $integrationType->id }}">
+                <i class="material-icons md-18 ml-10 mr-10">settings</i>
+                @{{ $t('general.taxPayer') }}
+            </b-nav-item>
+            <b-nav-item class="sub-menu" :to="{ name: 'documentList'}">
+                <i class="material-icons md-18 ml-10 mr-10">file_copy</i>
+                @{{ $t('commercial.documents') }}
+            </b-nav-item>
+            <b-nav-item class="sub-menu" :to="{ name: 'rateList'}">
+                <i class="material-icons md-18 ml-10 mr-10">public</i>
+                @{{ $t('commercial.exchangeRates') }}
+            </b-nav-item>
+
+            <b-dropdown-divider></b-dropdown-divider>
+
+            <b-nav-item class="sub-menu" href="/home" v-b-tooltip.hover :title="$t('general.changeTaxPayer')">
                 <i class="material-icons md-18 ml-10 mr-10">sync</i>
                 @{{ $t('general.changeTaxPayer') }}
             </b-nav-item>
-    </div>
-</li>
+        </b-dropdown>
+    </li>
 
     <li class="nav-item">
         @php
-            $variant = ($currentCycle->year == \Carbon\Carbon::now()->year) ? 'primary' : 'danger';
+            $variant = ($currentCycle->year == \Carbon\Carbon::now()->year) ? 'outline-primary' : 'outline-danger';
         @endphp
-        <b-dropdown id="dropdown-1" text="{{ $currentCycle->year }}" class="m-md-2" variant="{{ $variant }}" size="sm">
+
+        <b-dropdown id="dropdown-1" text="{{ $currentCycle->year }}" class="m-md-2" variant="{{ $variant }}"  size="sm">
             @foreach ($cycleData as $cycle)
                 <b-dropdown-item href="/{{ $taxPayerData->id }}/{{ $cycle->id }}/">
                     <i class="material-icons md-18 ml-10 mr-10">calendar_today</i>
@@ -67,30 +71,5 @@
                 @{{ $t('general.showMore') }}
             </b-dropdown-item>
         </b-dropdown>
-    
-        <div class="dropdown-menu dropdown-menu-middle" aria-labelledby="dropdownCycle">
-            <h3 class="nav-heading sub">
-                Change Cycle
-            </h3>
-            <b-nav vertical>
-                @foreach ($cycleData as $cycle)
-                    <b-nav-item class="sub-menu" href="/{{ $taxPayerData->id }}/{{ $cycle->id }}/">
-                        <i class="material-icons md-18 ml-10 mr-10">calendar_today</i>
-                        {{ $cycle->year }}
-                    </b-nav-item>
-                @endforeach
-                <b-nav-item class="sub-menu" :to="{ name: 'cycleList'}">
-                    <i class="material-icons md-18 ml-10 mr-10">more_horiz</i>
-                    @{{ $t('general.showMore') }}
-                </b-nav-item>
-            </b-nav>
-        </div>
-    </li>
-    
-    <li class="nav-item">
-        <b-link href="/home" v-b-tooltip.hover :title="$t('general.changeTaxPayer')" class="float-right nav-heading sub">
-            <i class="material-icons md-14"> sync </i>
-            <small>@{{ $t('general.change') }}</small>
-        </b-link>
     </li>
 @endisset
