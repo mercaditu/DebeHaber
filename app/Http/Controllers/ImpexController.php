6df74c7a2b6e7e6add50ef051e4a43c4ba61c3fd
Abhishek Shah <abhi@cognitivo.in>
2019-04-18 07:10:08 -0500

    fixed Journal create controller

--- app/Http/Controllers/ImpexController.php
+++ app/Http/Controllers/ImpexController.php
@@ -13,86 +13,67 @@ use Illuminate\Http\Request;
 class ImpexController extends Controller
 {
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(Taxpayer $taxPayer, Cycle $cycle)
     {
         return GeneralResource::collection(
             Impex::with('transactions')
-            ->with('expenses')
-            ->whereBetween('date', [$cycle->start_date, $cycle->end_date])
-            ->orderBy('date', 'desc')
-            ->paginate(50)
+                ->with('expenses')
+                ->whereBetween('date', [$cycle->start_date, $cycle->end_date])
+                ->orderBy('date', 'desc')
+                ->paginate(50)
         );
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-       $impex = Impex::where('id',$request->id)->with('expenses')->first();
-       if($impex != null)
-       {
-        $transaction = Transaction::where('number',$request->details[0]['number'])->with('details')->first();
+        $impex = Impex::where('id', $request->id)->with('expenses')->first();
+        $transaction = Transaction::where('number', $request->details[0]['number'])->with('details')->first();
         $impex->save();
-        foreach ($request->expenses as $expense) {
-         $detail=$transaction->details()->where('chart_id',$expense['chart_id'])->first();
-         if($detail != null )
-         {
-             $impexExpense= ImpexExpense::where('id', $expense['id'])->first();
-             $impexExpense->impex_id = $impex->id;
-             $impexExpense->chart_id = $detail->chart_id;
-             $impexExpense->transaction_detail_id = $detail->id;
-             $impexExpense->value = $detail->value;
-             $impexExpense->currency = $transaction->currency;
-             $impexExpense->rate = $transaction->rate;
-             $impexExpense->save();
-         }
-         else{
-             $impexExpense= ImpexExpense::where('id', $expense['id'])->first();
-             $impexExpense->impex_id = $impex->id;
-             $impexExpense->chart_id = $expense['chart_id'];
-             $impexExpense->transaction_detail_id = null;
-             $impexExpense->value = $expense['value'];
-             $impexExpense->currency = $expense['currency'];
-             $impexExpense->rate = $expense['rate'];
-             $impexExpense->save();
-         }
-         
-        }
-       }
-       
 
+        foreach ($request->expenses as $expense) {
+            $detail = $transaction->details()->where('chart_id', $expense['chart_id'])->first();
+            $impexExpense = ImpexExpense::where('id', $expense['id'])->first();
+            $impexExpense->chart_id = $expense['chart_id'];
+            $impexExpense->transaction_detail_id = $detail->id;
+            $impexExpense->value = $detail->value;
+            $impexExpense->currency = $transaction->currency;
+            $impexExpense->rate = $transaction->rate;
+            $impexExpense->save();
+        }
     }
 
     /**
-    * Display the specified resource.
-    *
-    * @param  \App\Impex  $impex
-    * @return \Illuminate\Http\Response
-    */
-    public function show(Taxpayer $taxPayer, Cycle $cycle,$impex)
+     * Display the specified resource.
+     *
+     * @param  \App\Impex  $impex
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Taxpayer $taxPayer, Cycle $cycle, $impex)
     {
         return new GeneralResource(
             Impex::where('id', $impex)
-            ->with('transactions')
-            ->with('expenses')
-            ->first()
+                ->with('transactions')
+                ->with('expenses')
+                ->first()
         );
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  \App\Impex  $impex
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Impex  $impex
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(Impex $impex)
     {
         //
--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -22,27 +22,29 @@ use Laravel\Spark\Contracts\Repositories\NotificationRepository;
 class JournalController extends Controller
 {
     /**
-    * Display a listing of the resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(Taxpayer $taxPayer, Cycle $cycle)
     {
         return GeneralResource::collection(
-            Journal::with(['details:journal_id,chart_id,debit,credit',
-            'details.chart:id,name,code,type'])
-            ->orderBy('date', 'desc')
-            ->paginate(50)
+            Journal::with([
+                'details:journal_id,chart_id,debit,credit',
+                'details.chart:id,name,code,type'
+            ])
+                ->orderBy('date', 'desc')
+                ->paginate(50)
         );
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return \Illuminate\Http\Response
-    */
-    public function store(Request $request,Taxpayer $taxPayer,Cycle $cycle)
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Taxpayer $taxPayer, Cycle $cycle)
     {
         $journal = $request->id == 0 ? new Journal() : Journal::where('id', $request->id)->first();
 
@@ -50,46 +52,48 @@ class JournalController extends Controller
         $journal->number = $request->number;
         $journal->comment = $request->comment ?? '';
         $journal->cycle_id = $cycle->id;
-        $journal->template_id = $request->template_id;
+        $journal->is_automatic = 0;
+        // $journal->template_id = $request->template_id;
 
         $journal->save();
 
-        foreach ($request->details as $detail)
-        {
-            $journalDetail = JournalDetail::firstOrNew(['id' => $detail['id']]);;
-            $journalDetail->journal_id = $journal->id;
-            $journalDetail->chart_id = $detail['chart_id'];
-            $journalDetail->debit = $detail['debit'] ?? 0;
-            $journalDetail->credit = $detail['credit'] ?? 0;
-            $journalDetail->save();
-        }
+        foreach ($request->details as $detail) {
+                $journalDetail = JournalDetail::firstOrNew(['id' => $detail['id']]);;
+                $journalDetail->journal_id = $journal->id;
+                $journalDetail->chart_id = $detail['chart_id'];
+                $journalDetail->debit = $detail['debit'] ?? 0;
+                $journalDetail->credit = $detail['credit'] ?? 0;
+                $journalDetail->save();
+            }
 
         return response()->json('Ok', 200);
     }
 
     /**
-    * Display the specified resource.
-    *
-    * @param  \App\Journal  $journal
-    * @return \Illuminate\Http\Response
-    */
+     * Display the specified resource.
+     *
+     * @param  \App\Journal  $journal
+     * @return \Illuminate\Http\Response
+     */
     public function show(Taxpayer $taxPayer, Cycle $cycle, $journalId)
     {
         return new GeneralResource(
-            Journal::with(['details',
-            'details.chart:id,name,code,type'])
-            ->where('id', $journalId)
-            ->orderBy('date', 'desc')
-            ->first()
+            Journal::with([
+                'details',
+                'details.chart:id,name,code,type'
+            ])
+                ->where('id', $journalId)
+                ->orderBy('date', 'desc')
+                ->first()
         );
     }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  \App\Journal  $journal
-    * @return \Illuminate\Http\Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Journal  $journal
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(Journal $journal)
     {
         //clean up use of journal in transactions and other places
