0dc0857b568104b51cb66f9adab6f025f88d8797
Abhishek Shah <abhi@cognitivo.in>
2019-04-23 18:06:14 -0400

    Update 2019_04_23_125247_update_impex_journal.php

--- database/migrations/2019_04_23_125247_update_impex_journal.php
+++ database/migrations/2019_04_23_125247_update_impex_journal.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateJournal extends Migration
+class UpdateImpexJournal extends Migration
 {
     /**
      * Run the migrations.
