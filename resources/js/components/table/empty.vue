416004e7b62cae207c7461526d782256a28ed8a5
Abhishek Shah <abhi@cognitivo.in>
2019-04-25 06:57:57 -0400

    updated empty animation

--- resources/js/components/table/empty.vue
+++ resources/js/components/table/empty.vue
@@ -7,7 +7,22 @@
         <router-link :to="{ name: formURL, params: { id: 0}}">creating</router-link>some data
       </p>
     </div>
-    <b-img thumbnail fluid width="256" src="/img/apps/no-data.svg" alt="Thumbnail"/>
+    <!-- <div style="width:100%;height:0;padding-bottom:75%;position:relative;">
+      <iframe
+        src="https://giphy.com/embed/12iY9OZoYcUabK"
+              src="https://media.giphy.com/media/12iY9OZoYcUabK/giphy.gif"
+        width="100%"
+        height="100%"
+        style="position:absolute"
+        frameborder="0"
+        class="giphy-embed"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <p>
+      <a href="https://giphy.com/gifs/results-12iY9OZoYcUabK">via GIPHY</a>
+    </p>-->
+    <b-img fluid center src="/img/apps/animasi-emptystate.gif"/>
   </div>
 </template>
 <script>
