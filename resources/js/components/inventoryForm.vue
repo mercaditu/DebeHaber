c5755a2fa74f1c648c4362b854c9a41bec90d357
pankeel <pankeel@cognitivo.in>
2019-05-08 19:13:36 +0530

    inventory component

--- resources/js/components/inventoryForm.vue
+++ resources/js/components/inventoryForm.vue
@@ -45,61 +45,21 @@ export default {
   components: { crud: crud },
   data() {
     return {
-      data: {
-        date: "",
-        details: [],
-        id: 0,
-        number: "",
-        comment: "",
-        template_id: "",
-        template: "",
-        value: ""
-      },
-      pageUrl: "/accounting/journals",
+     data: {
+       start_date:'',
+       end_date:'',
+       sales_vale:'',
+       cost_vale:'',
+       chart_id:''
+     },
+      charts:[],
+    
 
-      accountCharts: [],
-
-      templates: [],
-
-      lastDeletedRow: []
+     
     };
   },
   computed: {
-    columns() {
-      return [
-        {
-          key: "chart_id",
-          label: this.$i18n.t("commercial.account"),
-          sortable: true
-        },
-        {
-          key: "credit",
-          label: this.$i18n.t("general.credit"),
-          sortable: true
-        },
-        {
-          key: "debit",
-          label: this.$i18n.t("general.debit"),
-          sortable: true
-        },
-        {
-          key: "actions",
-          label: "",
-          sortable: false
-        }
-      ];
-    },
-    Balance() {
-      var debit = 0;
-      var credit = 0;
-      this.data.details.forEach(e => {
-        debit += e.debit;
-        credit += e.credit;
-        console.log(e);
-      });
-
-      return debit - credit;
-    },
+  
 
     baseUrl() {
       return (
@@ -107,149 +67,18 @@ export default {
       );
     }
   },
-  methods: {
-    onSave() {
-      var app = this;
-
-      if (app.Balance < 0) {
-        crud.methods
-          .onUpdate(app.baseUrl + app.pageUrl, app.data)
-          .then(function(response) {
-            app.$snack.success({
-              text: app.$i18n.t("commercial.JournalSaved")
-            });
-            app.$router.go(-1);
-          })
-          .catch(function(error) {
-            app.$snack.danger({ text: "Error OMG!: " + error });
-          });
-      }
-    },
-
-    onSaveNew() {
-      var app = this;
-
-      if (app.Balance === 0) {
-        crud.methods
-          .onUpdate(app.baseUrl + app.pageUrl, app.data)
-          .then(function(response) {
-            app.$snack.success({
-              text: app.$i18n.t("commercial.JournalSaved")
-            });
-            (app.data.date = ""),
-              (app.data.details = []),
-              (app.data.id = 0),
-              (app.data.number = ""),
-              (app.data.comment = ""),
-              (app.data.template_id = ""),
-              (app.data.template = ""),
-              (app.data.value = "");
-            app.$router.push({ name: app.$route.name, params: { id: "0" } });
-          })
-          .catch(function(error) {
-            app.$snack.danger({
-              text: this.$i18n.t("general.errorMessage")
-            });
-          });
-      }
-    },
-
-    onCancel() {
-      this.$swal
-        .fire({
-          title: this.$i18n.t("general.cancel"),
-          text: this.$i18n.t("general.cancelVerification"),
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonText: this.$i18n.t("general.cancelConfirmation"),
-          cancelButtonText: this.$i18n.t("general.cancelRejection")
-        })
-        .then(result => {
-          if (result.value) {
-            this.$router.go(-1);
-          }
-        });
-    },
-
-    addDetailRow() {
-      this.data.details.push({
-        // index: this.data.details.length + 1,
-        id: 0,
-        chart_id: this.accountCharts[0].id,
-        debit: "0",
-        credit: "0"
-      });
-    },
-
-    deleteRow(item) {
-      if (item.id > 0) {
-        var app = this;
-
-        crud.methods
-          .onDelete(app.baseUrl + app.pageUrl + "/details", item.id)
-          .then(function(response) {});
-      }
-
-      this.lastDeletedRow = item;
-
-      this.$snack.success({
-        text: this.$i18n.t("general.rowDeleted"),
-        button: this.$i18n.t("general.undo"),
-        action: this.undoDeletedRow
-      });
-
-      this.data.details.splice(this.data.details.indexOf(item), 1);
-    },
-
-    undoDeletedRow() {
-      if (this.lastDeletedRow.id > 0) {
-        crud.methods
-          .onUpdate(app.baseUrl + app.pageUrl + "/details", this.lastDeletedRow)
-          .then(function(response) {});
-        //axios code to insert detail again??? or let save do it.
-      }
-
-      this.data.details.push(this.lastDeletedRow);
-    },
-
-    onGenerateDetail() {
-      var app = this;
-      app.data.template_id = app.data.template.id;
-      app.data.template.details.forEach(element => {
-        app.data.details.push({
-          id: 0,
-          chart_id: element.chart_id,
-          debit: app.data.value * element.debit_coef,
-          credit: app.data.value * element.credit_coef
-        });
-      });
-    }
-  },
 
   mounted() {
     var app = this;
 
-    if (app.$route.params.id > 0) {
-      crud.methods
-        .onRead(app.baseUrl + app.pageUrl + "/" + app.$route.params.id)
-        .then(function(response) {
-          app.data = response.data.data;
-        });
-    } else {
-      app.data.date = new Date(Date.now()).toISOString().split("T")[0];
-    }
 
     crud.methods
-      .onRead(app.baseUrl + "/accounting/charts/for/accountables/")
+      .onRead(app.baseUrl + "/accounting/charts/for/income/")
       .then(function(response) {
-        app.accountCharts = response.data.data;
+        app.charts = response.data.data;
       });
 
-    crud.methods
-      .onRead(app.baseUrl + "/accounting/journal-templates")
-      .then(function(response) {
-        app.templates = response.data.data;
-      });
+   
   }
 };
 </script>
