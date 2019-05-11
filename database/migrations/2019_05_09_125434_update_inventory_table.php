3d9093e58cc3f1f3a4f992b9336e7fe6965d55e9
Abhishek Shah <abhi@cognitivo.in>
2019-05-10 21:47:10 -0400

    Update 2019_05_09_125434_update_inventory_table.php

--- database/migrations/2019_05_09_125434_update_inventory_table.php
+++ database/migrations/2019_05_09_125434_update_inventory_table.php
@@ -14,7 +14,8 @@ class UpdateInventoryTable extends Migration
     public function up()
     {
         Schema::table('inventories', function (Blueprint $table) {
-            $table->unsignedInteger('chart_sales_id')->after('chart_id');
+            $table->unsignedInteger('chart_income_id')->after('chart_id');
+            $table->unsignedInteger('chart_expense_id')->after('chart_income_id');
             $table->unsignedInteger('journal_id')->after('chart_sales_id');
             $table->unsignedDecimal('discount_value', 18, 2)->default(0)->after('inventory_value');
             $table->dropColumn('cost_value');
