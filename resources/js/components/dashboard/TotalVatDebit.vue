3d5dbbb9d6051b9f88830b291c975eece671e9f3
Pankeel Shah <pankeel@cognitivo.in>
2020-08-22 14:54:21 +0530

    update

--- resources/js/components/dashboard/TotalVatDebit.vue
+++ resources/js/components/dashboard/TotalVatDebit.vue
@@ -1,53 +1,44 @@
 <template>
-    <GChart :type="selectedType" :data="chartData" :options="chartOptions" />
+  <GChart :type="selectedType" :data="chartData" :options="chartOptions" />
 </template>
 <script>
-import crud from '../../components/crud.vue';
+import crud from "../../components/crud.vue";
 
 export default {
-    props: ['type', 'startDate', 'endDate'],
-    components: { 'crud': crud },
-    data () {
-        return {
-            // Array will be automatically processed with visualization.arrayToDataTable function
-            chartData: [
-                ['Item', 'Total']
-            ],
-            selectedType: "PieChart",
-            chartTypes: [
-                "PieChart",
-                "LineChart",
-                "BarChart"
-            ],
-            chartOptions: {
-                chart: {
-                    title: 'Breakdown',
-                    sliceVisibilityThreshold: 0.13
-                }
-            }
+  props: ["type", "startDate", "endDate"],
+  components: { crud: crud },
+  data() {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [["Item", "Total"]],
+      selectedType: "PieChart",
+      chartTypes: ["PieChart", "LineChart", "BarChart"],
+      chartOptions: {
+        chart: {
+          title: "Breakdown",
+          sliceVisibilityThreshold: 0.13
         }
-    },
+      }
+    };
+  },
 
-    computed: {
-        baseUrl() {
-            var app = this;
-            return '/api/' + app.$route.params.taxPayer + '/kpi/totalVat/';
-        },
-    },
-
-    mounted() {
-        var app = this;
-        //do something after mounting vue instance
-        // alert(app.baseUrl + '/transactions/' + this.type + '/' + this.startDate + '/' + this.endDate);
-        crud.methods
-        .onRead(app.baseUrl)
-        .then(function (response) {
-            console.log(response);
-            response.data.data.forEach(element => {
-                app.chartData.push([element.Item,Number(element.Total)])
-            });
-
-        });
+  computed: {
+    baseUrl() {
+      var app = this;
+      return "/api/" + app.$route.params.taxPayer + "/kpi/totalVat";
     }
-}
+  },
+
+  mounted() {
+    var app = this;
+    //do something after mounting vue instance
+    // alert(app.baseUrl + '/transactions/' + this.type + '/' + this.startDate + '/' + this.endDate);
+    crud.methods.onRead(app.baseUrl).then(function(response) {
+      console.log(response);
+      response.data.data.forEach(element => {
+        app.chartData.push([element.Item, Number(element.Total)]);
+      });
+    });
+  }
+};
 </script>
--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -1,36 +1,39 @@
-
 <template>
-    <div>
-        <b-card-group columns>
-            <b-card no-body>
-                <pie-transaction-items startDate="2018-01-01" endDate="2019-01-01" type="1"></pie-transaction-items>
-            </b-card>
+  <div>
+    <b-card-group columns>
+      <b-card no-body>
+        <pie-transaction-items
+          startDate="2018-01-01"
+          endDate="2019-01-01"
+          type="1"
+        ></pie-transaction-items>
+      </b-card>
 
-            <b-card no-body>
-               <bar-transaction-date startDate="2018-01-01" endDate="2019-01-01" type="1"></bar-transaction-date>
-            </b-card>
-                
-        </b-card-group>
-          <b-card-group columns>
-            <b-card  no-body>
-                <total-vat-debit></total-vat-debit>
-            </b-card>
+      <b-card no-body>
+        <bar-transaction-date
+          startDate="2018-01-01"
+          endDate="2019-01-01"
+          type="1"
+        ></bar-transaction-date>
+      </b-card>
+    </b-card-group>
+    <b-card-group columns>
+      <b-card no-body>
+        <total-vat-debit></total-vat-debit>
+      </b-card>
 
-             <b-card  no-body>
-                <total-vat-credit></total-vat-credit>
-            </b-card>
-           
-        </b-card-group>
-    </div>
+      <b-card no-body>
+        <total-vat-credit></total-vat-credit>
+      </b-card>
+    </b-card-group>
+  </div>
 </template>
 
 <script>
 // import i18n from '../plugins/i18n';
-export default
-{
-    data() {
-        return {
-        };
-    },
-}
+export default {
+  data() {
+    return {};
+  }
+};
 </script>
