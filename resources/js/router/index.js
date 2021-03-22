ecf6f8c556d5a71aaae960ed6cc2a30126822d7f
Pankeel Shah <pankeelshah_9999@gmail.com>
2021-03-22 16:41:24 +0530

    Update CreditNote

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -254,7 +254,7 @@ export default [
                 meta: {
                     title: "commercial.creditNotes"
                 },
-
+                url: "sales/upload",
                 label: "general.upload",
                 icon: "cloud_upload",
                 variant: "dark"
